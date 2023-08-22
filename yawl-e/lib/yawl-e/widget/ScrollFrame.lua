f50abaf99171776f8b8e1f617493758d264ec7cc
Renno231 <75190549+Renno231@users.noreply.github.com>
2023-08-22 11:54:22 -0500

    Create ScrollFrame.lua

---
+++ yawl-e/lib/yawl-e/widget/ScrollFrame.lua
@@ -0,0 +1,39 @@
+local class = require("libClass2")
+local Frame = require("yawl-e.widget.Frame")
+
+local ScrollFrame = class(Frame) 
+    --extends frame object
+function ScrollFrame:scrollX(num)
+    checkArg(1, num, 'number', 'nil')
+    local oldValue = self._scrollindexX or 0
+    if (num) then self._scrollindexX = (self._scrollindexX or oldValue) + num end
+    return math.floor(0.5+ oldValue)
+end
+
+function ScrollFrame:scrollY(num)
+    checkArg(1, num, 'number', 'nil')
+    local oldValue = self._scrollindexY or 0
+    if (num) then self._scrollindexY = (self._scrollindexY or oldValue) + num end
+    return math.floor(0.5+ oldValue)
+end
+--[[
+---@return number
+function ScrollFrame:absX()
+    if (self._parentFrame) then
+        return self._parentFrame:absX() + self:x() - 1 + self:scrollX()
+    else
+        return self:x()
+    end
+end
+
+---Get the absolute y position on screen.
+---@return number
+function ScrollFrame:absY()
+    if (self._parentFrame) then
+        return self._parentFrame:absY() + self:y() - 1 + self:scrollY()
+    else
+        return self:y()
+    end
+end
+]]
+return ScrollFrame
