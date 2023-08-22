d3b7ae1034869bbedfce38b03b08c91f1b207def
Renno231 <75190549+Renno231@users.noreply.github.com>
2023-08-21 23:43:11 -0500

    loads libraries as need instead of preloading

--- yawl-e/lib/yawl-e/widget/init.lua
+++ yawl-e/lib/yawl-e/widget/init.lua
@@ -1,5 +1,5 @@
 local yawle = {
-    Widget       = require('yawl-e.widget.Widget'),
+    --[[Widget       = require('yawl-e.widget.Widget'),
     Rectangle    = require('yawl-e.widget.Rectangle'),
     Frame        = require('yawl-e.widget.Frame'),
     Text         = require("yawl-e.widget.Text"),
@@ -13,11 +13,27 @@ local yawle = {
     SliderBar    = require("yawl-e.widget.SliderBar"),
     ProgressBar  = require("yawl-e.widget.ProgressBar"),
     SortedList   = require("yawl-e.widget.SortedList"),
-    Button       = require("yawl-e.widget.Button")
+    Button       = require("yawl-e.widget.Button"),
+    DropdownList = require("yawl-e.widget.DropdownList")]]
 }
-
+setmetatable(yawle,{
+    __index = function(table, index, value)
+        local lib = require("yawl-e.widget."..index)
+        if not lib.Class then lib.Class = index end
+        if not rawget(yawle, index) then rawset(yawle, index, lib) end
+        return lib
+    end
+})
+--[=[
 for class, object in pairs (yawle) do
+    --[[
+        local object = require("yawl-e.widget."..object)
+        yawle[class] = object
+        object.Class = class 
+    ]]
     object.Class = class
-end
+end]=]
+
+--could potentially make metamethod that loads these as needed instead of all at once (reduces memory cost when not all things are utilized)
 
 return yawle
