8402b3b607860e5901eea81dda57447c9f57c73f
Renno231 <75190549+Renno231@users.noreply.github.com>
2024-01-17 20:35:27 -0600

    added minimum and maximum values for ScrollFrame and descendants

--- yawl-e/lib/yawl-e/widget/ScrollFrame.lua
+++ yawl-e/lib/yawl-e/widget/ScrollFrame.lua
@@ -12,6 +12,22 @@ function ScrollFrame:scrollX(num, override)
     return oldValue
 end
 
+function ScrollFrame:minimumScrollX(num)
+    checkArg(1, num, 'number', 'boolean', 'nil')
+    local oldValue = self._minimumScrollX
+    if type(num) == "number" then self._minimumScrollX = num end
+    if num == false then self._minimumScrollX = nil end
+    return oldValue
+end
+
+function ScrollFrame:maximumScrollX(num)
+    checkArg(1, num, 'number', 'boolean', 'nil')
+    local oldValue = self._maximumScrollX
+    if type(num) == "number" then self._maximumScrollX = num end
+    if num == false then self._maximumScrollX = nil end
+    return oldValue
+end
+
 function ScrollFrame:scrollY(num, override)
     checkArg(1, num, 'number', 'nil')
     checkArg(2, override, 'boolean','nil')
@@ -19,5 +35,22 @@ function ScrollFrame:scrollY(num, override)
     if (num) then self._scrollindexY = override and num or ((self._scrollindexY or oldValue) + num) end
     return oldValue
 end
+
+function ScrollFrame:minimumScrollY(num)
+    checkArg(1, num, 'number', 'boolean', 'nil')
+    local oldValue = self._minimumScrollY
+    if type(num) == "number" then self._minimumScrollY = num end
+    if num == false then self._minimumScrollY = nil end
+    return oldValue
+end
+
+function ScrollFrame:maximumScrollY(num)
+    checkArg(1, num, 'number', 'boolean', 'nil')
+    local oldValue = self._maximumScrollY
+    if type(num) == "number" then self._maximumScrollY = num end
+    if num == false then self._maximumScrollY = nil end
+    return oldValue
+end
+
 
 return ScrollFrame
