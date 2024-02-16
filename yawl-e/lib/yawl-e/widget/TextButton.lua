73c4cd80d4f8fef169f290e468d09444fd4e939e
Renno231 <75190549+Renno231@users.noreply.github.com>
2024-02-15 23:32:30 -0600

    TextButton:new bugfix

--- yawl-e/lib/yawl-e/widget/TextButton.lua
+++ yawl-e/lib/yawl-e/widget/TextButton.lua
@@ -1,6 +1,7 @@
 --text class object with a :value() and defaultCallback for touch
 local Text = require("yawl-e.widget.Text")
 local TextButton = require("libClass2")(Text)
+local unicode = require("unicode")
 
 function TextButton:new(parent, x, y, width, height, text, foregroundColor)
     checkArg(1, parent, 'table')
@@ -8,13 +9,13 @@ function TextButton:new(parent, x, y, width, height, text, foregroundColor)
     checkArg(3, y, 'number', 'nil')
     checkArg(4, text, 'string')
     checkArg(5, foregroundColor, 'number', 'nil')
-    checkArg(4, width, 'number')
+    checkArg(4, width, 'number', 'nil')
     checkArg(5, height, 'number')
     if (type(x) == "table") then checkArg(3, y, 'nil') else checkArg(3, y, 'number') end
     local o = self.parent(parent, x, y, text, foregroundColor)
     setmetatable(o, {__index = self})
     ---@cast o Text
-    o:size(width, height)
+    o:size(width or unicode.len(self._parsedText and self._parsedText[1] or text or " "), height)
     o:textHorizontalAlignment("center")
     o:textVerticalAlignment("center")
     o:textOffset("center")
